99fabeb3b29ef44e5c25a474ca0d447684ba63ce
Shemvick <fasholasemilore@gmail.com>
2024-11-01 20:29:46 +0100

    added files

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,35 @@
-const { defineConfig } = require("cypress");
+// const { defineConfig } = require("cypress");
 
+// module.exports = defineConfig({
+//   e2e: {
+//     setupNodeEvents(on, config) {
+//       // implement node event listeners here
+//     },
+//   },
+// });
+
+const { defineConfig } = require("cypress");
+const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
+const browserify = require("@badeball/cypress-cucumber-preprocessor/browserify");
+
+
+async function setupNodeEvents(on, config) {
+  await preprocessor.addCucumberPreprocessorPlugin(on, config);
+  on("file:preprocessor", browserify.default(config));
+  return config;
+}
 module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+  defaultCommandTimeout: 10000,
+  env: {
+    baseURL: "https://www.demoblaze.com/",
   },
+
+  e2e: {
+    setupNodeEvents,
+    specPattern: 'cypress/integration/*.feature',
+
+  },
+
 });
+
+
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,57 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('navigateToPage', (url) => {
+  cy.visit(url);  
+  cy.wait(10000);
+  cy.get('#login2').should('be.visible');  
+});
+
+Cypress.Commands.add('clickOnElement', (selector, text) => {
+  cy.get(selector).contains(text).click(); 
+  cy.wait(4000);
+  cy.get('#logInModal').should('be.visible');  
+  cy.wait(4000);  
+});
+
+
+Cypress.Commands.add("login", (Username, Password) => {
+  cy.wait(2000);
+  cy.get("#loginusername").type(Username);
+  cy.wait(2000);
+  cy.get("#loginpassword").type(Password);
+  cy.wait(4000);
+});
+
+Cypress.Commands.add("login", (invalidUsername, Password) => {
+  cy.wait(2000);
+  cy.get("#loginusername").type(invalidUsername);
+  cy.wait(2000);
+  cy.get("#loginpassword").type(Password);
+  cy.wait(4000);
+});
+
+
+Cypress.Commands.add("login", (Username, invalidPassword) => {
+  cy.wait(2000);
+  cy.get("#loginusername").type(Username);
+  cy.wait(2000);
+  cy.get("#loginpassword").type(invalidPassword);
+  cy.wait(4000);
+});
+
+Cypress.Commands.add("login", (invalidUsername, invalidPassword) => {
+  cy.wait(2000);
+  cy.get("#loginusername").type(invalidUsername);
+  cy.wait(2000);
+  cy.get("#loginpassword").type(invalidPassword);
+  cy.wait(4000);
+});
+
+Cypress.Commands.add('clickOnElement', (selector, text) => {
+  cy.get(selector).contains(text).click(); 
+  cy.get('.btn.btn-primary').contains('Log in').click()  
+});
+
+
